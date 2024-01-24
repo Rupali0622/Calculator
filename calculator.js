cd41d7adb54cd8ab89f4d11d000b37ac088f2d4a
Rupali <rupalichauhan022@gmail.com>
2024-01-24 23:27:59 +0530

    calculator

--- calculator.js
+++ calculator.js
@@ -1,5 +1,31 @@
-let boxes=document.querySelector("button");
+let boxes=document.querySelectorAll("button");
+let screen=document.querySelector(".display");
+let add=document.querySelector(".add");
+let num1=10;
+let num2=5;
 
-boxes.addEventListener("click",(box)=>{
-    console.log("I was Clicked");
+boxes.forEach((box)=>{
+    box.addEventListener("click",()=>{
+        box.style.color="black";
+    });
 });
+
+screen.addEventListener("mouseover",myFunction);
+
+const myFunction=()=> {
+    document.getElementsByClassName("display").value = addition();
+  }
+
+const addition=()=>{
+    return num1+num2;
+};
+const subtraction=()=>{
+    return num1-num2;
+};
+const multiplication=()=>{
+    return num1*num2;
+};
+const division=()=>{
+    return num1/num2;
+};
+
